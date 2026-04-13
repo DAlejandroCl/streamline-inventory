59620ba989cb43e5aace11668b50c030120d5243
Diego <alejandro1clavijo3@gmail.com>
2026-04-13 17:42:05 -0500

    refactor: remove Jest and migrate to Node native test runner with tsx

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+
+  roots: ['<rootDir>/src/tests'],
+
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json'
+      }
+    ]
+  },
+
   moduleFileExtensions: ['ts', 'js'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  }
+
+  testMatch: ['**/*.test.ts']
 };
