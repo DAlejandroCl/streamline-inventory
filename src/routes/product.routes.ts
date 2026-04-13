0b6bf9ebe28215225fc60363173d3ef883248c3d
Diego <alejandro1clavijo3@gmail.com>
2026-04-13 17:02:59 -0500

    feat: add validation and improve product routes

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,20 +1,68 @@
 import { Router } from "express";
+import { body, param } from "express-validator";
 import {
   getProducts,
+  getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
-  patchProduct,
+  patchProduct
 } from "../controllers/product.controllers.js";
+import { handleInputErrors } from "../middlewares/validation.js";
 
 // ROUTER SETUP
 const router = Router();
 
-// ROUTES
-router.get("/", getProducts);
-router.post("/", createProduct);
-router.put("/:id", updateProduct);
-router.patch("/:id", patchProduct);
-router.delete("/:id", deleteProduct);
+// GET ALL
+router.get('/', getProducts);
+
+// GET BY ID
+router.get(
+  '/:id',
+  param('id').isInt().withMessage('ID must be a number'),
+  handleInputErrors,
+  getProductById
+);
+
+// CREATE
+router.post(
+  '/',
+  body('name')
+    .notEmpty()
+    .withMessage('Product name is required'),
+
+  body('price')
+    .notEmpty()
+    .withMessage('Price is required')
+    .isNumeric()
+    .withMessage('Price must be a number'),
+
+  handleInputErrors,
+  createProduct
+);
+
+// UPDATE (PUT)
+router.put(
+  '/:id',
+  param('id').isInt().withMessage('ID must be a number'),
+  handleInputErrors,
+  updateProduct
+);
+
+// PATCH
+router.patch(
+  '/:id',
+  param('id').isInt().withMessage('ID must be a number'),
+  handleInputErrors,
+  patchProduct
+);
+
+// DELETE
+router.delete(
+  '/:id',
+  param('id').isInt().withMessage('ID must be a number'),
+  handleInputErrors,
+  deleteProduct
+);
 
 export default router;
