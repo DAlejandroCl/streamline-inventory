ade3d647a21236391fcd5165b3e73caed173802f
Diego <alejandro1clavijo3@gmail.com>
2026-04-14 19:28:15 -0500

    chore(middleware): add request validation handler using express-validator

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -2,11 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
 // VALIDATION MIDDLEWARE
-export const handleInputErrors = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const validate = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
