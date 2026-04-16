9dfe5435bc48ccb8f43d950f8403a52188d5a473
Diego <alejandro1clavijo3@gmail.com>
2026-04-16 15:49:51 -0500

    refactor(router): integrate createProductAction and clean duplicated logic

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../layouts/MainLayout";
+import MainLayout from "../layouts/MainLayout";
 import HomePage from "../pages/HomePage";
 import NewProductPage from "../pages/NewProductPage";
 import ProductsPage from "../pages/ProductsPage";
+import ErrorPage from "../pages/ErrorPage";
+
+import { createProductAction } from "../actions/product.actions";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -19,7 +23,8 @@ export const router = createBrowserRouter([
       },
       {
         path: "products/new",
-        element: <NewProductPage />
+        element: <NewProductPage />,
+        action: createProductAction
       }
     ]
   }
