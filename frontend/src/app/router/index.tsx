6b6d7110e925f09fd939a5a1392d834239ede0d6
Diego <alejandro1clavijo3@gmail.com>
2026-04-17 17:33:11 -0500

    refactor(router): fix imports and integrate product actions and loaders

--- frontend/src/app/router/index.tsx
+++ frontend/src/app/router/index.tsx
@@ -1,12 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import MainLayout from "../../layouts/MainLayout";
 import DashboardPage from "../../pages/DashboardPage";
 import ProductsPage from "../../pages/ProductsPage";
 import NewProductPage from "../../pages/NewProductPage";
+import EditProductPage from "../../pages/EditProductPage";
 import ErrorPage from "../../pages/ErrorPage";
+
+import {
+  productsLoader,
+  productByIdLoader,
+} from "../../features/products/loaders/products.loader";
+
 import { deleteProductAction } from "../../actions/deleteProduct.action";
-import { toggleAvailabilityAction } from "../../actions/toogleAvailability.action";
-import { productsLoader } from "../../features/products/loaders/products.loader";
+import { toggleAvailabilityAction } from "../../actions/toggleAvailability.action";
+import { updateProductAction } from "../../actions/updateProduct.action";
 
 export const router = createBrowserRouter([
   {
@@ -24,17 +32,23 @@ export const router = createBrowserRouter([
         loader: productsLoader,
       },
       {
+        path: "products/new",
+        element: <NewProductPage />,
+      },
+      {
+        path: "products/:id/edit",
+        element: <EditProductPage />,
+        loader: productByIdLoader,
+        action: updateProductAction,
+        errorElement: <ErrorPage />,
+      },
+      {
         path: "products/delete",
         action: deleteProductAction,
       },
       {
         path: "products/toggle",
         action: toggleAvailabilityAction,
-      },
-      {
-        path: "products/new",
-        element: <NewProductPage />,
-        errorElement: <ErrorPage />,
       },
     ],
   },
