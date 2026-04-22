473f36f33a80c8900238b52caf56baa34ada4538
Diego <alejandro1clavijo3@gmail.com>
2026-04-22 17:52:43 -0500

    feat: wrap RouterProvider with ThemeProvider in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
+import { ThemeProvider } from "./context/ThemeContext";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 );
