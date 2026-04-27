906e3f3c98f54fbc20d908b4824454ce40e9e7f1
Diego <alejandro1clavijo3@gmail.com>
2026-04-27 16:11:14 -0500

    feat: wrap app with SettingsProvider alongside ThemeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 import { ThemeProvider } from "./context/ThemeContext";
+import { SettingsProvider } from "./context/SettingsContext";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <SettingsProvider>
+        <RouterProvider router={router} />
+      </SettingsProvider>
     </ThemeProvider>
   </StrictMode>,
 );
